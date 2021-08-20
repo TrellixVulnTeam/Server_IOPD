8ec9c38fa514e4d49b00cbf494a8f23ab26f1e18
unknown <vt332141@gmail.com>
2021-08-20 21:41:50 +0700

    Install prettier,lint-staged,husky

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+      app.set('view engine', 'hbs');
+  app.set('views', path.join(__dirname, 'resources/views'));
 //console.log(path.join(__dirname, 'resources/views'));
 
 // Routes Init
